585a824754dbc03f1326771ddc95cc4d8c399ae6
mootez <mootez.bessifi@gmail.com>
2014-03-07 20:31:52 +0000

    restructure config-session branch

--- VyosSessionConfig/__init__.py
+++ VyosSessionConfig/__init__.py
@@ -1,20 +1,16 @@
 #!../bin/python
-
 import os
-import logging
 import ConfigParser
-#import ExecuterFormatter
 
-class sessionconfig:
 
-	shell_api_path = set_config_params('bin','shell_api_path')
+class sessionconfig():
 
 	def set_config_params(self,tag,key):
 		config = ConfigParser.SafeConfigParser()
 		config.readfp(open('../VyosApi.conf'))
 		return config.get(tag,key)
 
-	def setup_config_session(self):
+	def setup_config_session(self,shell_api_path):
                 return ("""
 			session_env=$(%s getSessionEnv $PPID) 
 			eval $session_env 
@@ -24,13 +20,7 @@ class sessionconfig:
 			if [ $? -ne 0 ]; then
         		echo "Something went wrong!"
 			fi
-		""" % (self.shell_api_path,self.shell_api_path,self.shell_api_path))
+		""" % (shell_api_path,shell_api_path,shell_api_path))
 
-	def commit(self):
-                call([], name="commit")
 
-	def save(self):
-                call([], name="save")
 
-	def discard():
-		call([], name="discard")
